5877d8b85c803a587df88c958f6d0d66f00b9a95
Alex Ensminger <ensmingeraa@gmail.com>
2016-04-28 00:34:20 -0600

    Final static todos

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 
-//GET /todos
+//GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos=todos;
@@ -30,6 +30,15 @@ app.get('/todos', function(req, res) {
 	else if (queryParams.hasOwnProperty('completed') && queryParams.completed==='false') {
 		filteredTodos=_.where(filteredTodos, {completed : false})
 	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length>0) {
+		
+
+		filteredTodos=_.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase())> -1;
+		});
+	}
+
 
 
 	res.json(filteredTodos);
