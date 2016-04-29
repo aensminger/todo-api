4d18562dd4711640931d002719f31429d4486cc7
Alex Ensminger <ensmingeraa@gmail.com>
2016-04-28 23:01:05 -0600

    add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var bcrypt = require('bcrypt');
-var _ =require('underscore');
+var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -11,11 +11,11 @@ module.exports = function(sequelize, DataTypes) {
 				isEmail: true
 			}
 		},
-		salt : {
-			type :DataTypes.STRING
+		salt: {
+			type: DataTypes.STRING
 		},
-		password_hash : {
-			type : DataTypes.STRING
+		password_hash: {
+			type: DataTypes.STRING
 		},
 		password: {
 			type: DataTypes.VIRTUAL,
@@ -23,10 +23,10 @@ module.exports = function(sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			},
-			set : function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
-				this.setDataValue('password',value);
+				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
@@ -40,11 +40,35 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		},
-		instanceMethods:  {
-			toPublicJSON : function () {
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email != 'string' && typeof body.password != 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+						resolve(user);
+
+					}, function(e) {
+						reject();
+					})
+				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'email', 'id', 'createdAt', 'updatedAt');
 			}
 		}
 	});
+
+	return user;
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require ('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -134,13 +135,12 @@ app.put('/todos/:id', function(req, res) {
 
 
 
-
 ////////////////////
 //USERS
 ////////////////////
 
 //post /users
-app.post('/users', function (req, res) {
+app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
@@ -149,8 +149,22 @@ app.post('/users', function (req, res) {
 		res.status(400).json(e);
 	});
 });
-var force=false;
-db.sequelize.sync({force : force}).then(function() {
+
+//post /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+	db.user.authenticate(body).then (function (user) {
+		res.json(user.toPublicJSON());
+	}, function (e) {
+		res.status(401).send();
+	})
+
+});
+
+var force = true;
+db.sequelize.sync({
+	force: force
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!!');
 	});
